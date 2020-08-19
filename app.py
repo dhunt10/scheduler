c72b3925e427f7b5ee7bcdfdcb6a04f2c497a9c7
Darin Hunt <darinhunt@Darins-MacBook-Pro-2.local>
2020-08-19 11:11:10 -0400

    adding endpoints

--- app.py
+++ app.py
@@ -1,35 +1,102 @@
-from flaskimport Flask, request, Response
+from flask import Flask, request, Response
 from flask_restful import Resource, Api, reqparse
+from main_class import Scheduler
+from person import person
 
 app = Flask(__name__)
 api = Api(app)
 
+model = Scheduler()
 
 class HealthCheck(Resource):
+
     def get(self):
-        return {'status': 'alive'}, 200
+        return {'status{}': 'alive{}'}, 200
 
 
 class get_avail_times(Resource):
     def __init__(self):
         """
+
         """
-        parser = reqparse.Requestparser(bundle_errors=True)
-        parse.add_argument('club',
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument('club',
                            type=str,
                            required=True,
                            help='What Club Are You A Member At')
-        parse.add_argument('date'
+        parser.add_argument('date',
                            type=str,
                            required=True,
                            help='Date is in MM/DD/YYYY format')
         self.parser = parser
 
 
-        def get(self):
-            """
-            """
+    def get(self):
+        """
+        """
 
-            args = self.parser.parse_args()
-            result = model.get_avail_times()
-            return result
+        args = self.parser.parse_args()
+        result = model.get_avail_times(club=args.club, date=args.date)
+        return result
+
+class make_appt(Resource):
+    def __init__(self):
+        parser = reqparse.RequestParser(bundle_erros=True)
+        parser.add_argument('name',
+                            type=str,
+                            required=True,
+                            help='name of applicant')
+        parser.add_argument('club',
+                            type=str,
+                            required=True,
+                            help='name of applicants club')
+        parser.add_argument('email',
+                            type=str,
+                            required=True,
+                            help='email of applicant')
+        parser.add_argument('phone',
+                            type=str,
+                            required=False,
+                            help='phone number of applicant')
+        parser.add_argument('date',
+                            type=str,
+                            required=True,
+                            help='Date is in MM/DD/YYYY format')
+        parser.add_argument('time',
+                            type=str,
+                            required=True,
+                            help='time is 24 hr format')
+
+        self.parser = parser
+
+    def get(self):
+        args = self.parser.parse_args()
+        customer = person()
+        customer.name = args.name
+        customer.club = args.club
+        customer.email = args.email
+        customer.phone = args.phone
+        result = model.make_appt(customer, time=args.time, date=args.date)
+        return result
+
+class get_dates(Resource):
+    def __init__(self):
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument('club',
+                            type=str,
+                            required=True,
+                            help='club of available dates')
+        self.parser = parser
+
+    def get(self):
+        args = self.parser.parse_args()
+        result = model.get_avail_dates(args.club)
+        return result
+
+api.add_resource(HealthCheck, '/healthcheck')
+api.add_resource(get_avail_times, '/at')
+api.add_resource(make_appt, '/ma')
+api.add_resource(get_dates, '/gd')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port='8080')
--- main_class.py
+++ main_class.py
@@ -3,13 +3,14 @@ import datetime
 from person import person
 import os
 
+
 class Scheduler(object):
 
     def __init__(self):
         """
 
         Initialization
-	"""
+        """
 
     def make_timeset(self, start, end):
         day = {}
@@ -37,10 +38,10 @@ class Scheduler(object):
         if curr_schedule == '':
             curr_schedule = dict()
             print("ye")
+        club = club.replace(" ", "_")
         if club not in curr_schedule:
             curr_schedule[club] = {}
         update_now(curr_schedule)
-
 
     def make_appt(self, person, time, date):
         curr_schedule = load_data()
@@ -52,3 +53,24 @@ class Scheduler(object):
             return "Confirmed for {}, at {}".format(date, time)
         else:
             return "Time is no longer available"
+
+    def get_avail_times(self, club, date):
+        open_dates = []
+        data = load_data()
+
+        date = date_to_data_form(date)
+
+        for i in data[club][date]:
+            if data[club][date][i] == None:
+                open_dates.append(i)
+
+        return open_dates
+
+    def get_avail_dates(self, club):
+        open_dates = []
+        data = load_data()
+
+        for i in data[club]:
+            open_dates.append(data_form_to_date(i))
+
+        return open_dates
